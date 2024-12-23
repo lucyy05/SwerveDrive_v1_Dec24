67323e9e115382e7033a7a1822465bfb47bf86ee
lucy <zihui0329@gmail.com>
2024-12-24 03:13:06 +0800

    temp commit by john, working on testbase

--- include/definitions.h
+++ include/definitions.h
@@ -38,12 +38,17 @@
 //#define RIGHT_ROTATION_SENSOR_PORT 1
 //#define IMU_PORT 17
 
+#define SLAM_DUNK_SENSOR_PORT 'A'
+#define SLAM_DUNK_SOLENOID 'B'
 
 #define POTENTIOMETER_SENSOR_PORT 'H'
-#define SOLENOID_SENSOR_PORT 'G'
+#define SOLENOID_SENSOR_PORT 'D'
+#define mobilegoal_bottom 'G'
 
 #define CONVEYOR_MOTOR 7
 #define ROLLER_MOTOR 6
+
+#define SLAM_DUNK_MOTOR 3
 
 #define ZERO_VECTOR INFINITY
 
@@ -66,6 +71,12 @@ pros::IMU imu(IMU_PORT);
 // pros::Motor liftL(LEFT_LIFT_MOTOR, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES);
 // pros::Motor liftR(RIGHT_LIFT_MOTOR, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
 
+pros::ADIDigitalOut slam_in_out(SLAM_DUNK_SOLENOID);
+pros::ADIAnalogIn slam_dunk(SLAM_DUNK_SENSOR_PORT);
+pros::ADIDigitalOut solenoid(SOLENOID_SENSOR_PORT);
+pros::ADIDigitalOut mobilegoal_bot(mobilegoal_bottom);
+pros::Motor slam_dunkkkk(SLAM_DUNK_MOTOR, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+
 
 pros::Rotation left_rotation_sensor(LEFT_ROTATION_SENSOR_PORT, true);
 pros::Rotation right_rotation_sensor(RIGHT_ROTATION_SENSOR_PORT, true);
@@ -84,7 +95,7 @@ extern "C" void vexGenericSerialBaudrate(  uint32_t index, uint32_t rate );
 extern "C" int32_t vexGenericSerialTransmit( uint32_t index, uint8_t *buffer, int32_t length );
 
 //Controllers
-int leftX = 0, leftY = 0, rightX = 0;
+int leftX = 0, leftY = 0, rightX = 0, rightY=0;
 
 //PARAMETERS
 const double DEADBAND =  8.0;
@@ -96,13 +107,10 @@ const double WHEEL_BASE_RADIUS = 161.50;   // mm
 const double MAX_SPEED = (2.0*M_PI*WHEEL_RADIUS*MAX_RPM)/60.0 ; //mm per second
 const double SPEED_TO_RPM = 60.0/(2.0*M_PI*WHEEL_RADIUS);
 const double MAX_ANGULAR = MAX_SPEED/WHEEL_BASE_RADIUS; // rad/s
-const double ACCEL = 8000.0;    // mm/s2
-const double ANGULAR_ACCEL = 1.0;   // rad/s2
-const double SCALING_FACTOR = MAX_RPM / 127.0;
+const double MAX_ANGULAR_SCALE = 0.8;
 const double TO_DEGREES = (180.0 / M_PI);
 const double TO_RADIANS = (M_PI / 180.0);
-const double THETA_MAX = 15.0;
-
+const double MAX_VOLTAGE = 12800;
 
 
 //moving (moveBase)
@@ -111,6 +119,13 @@ vector3D target_r;
 vector3D temp;
 vector3D v_right;
 vector3D v_left;
+
+
+//voltages
+int32_t lu; // left upper 
+int32_t ll; // left lower 
+int32_t ru; // right upper 
+int32_t rl; // right lower 
 
 const double angle_kP_left = 20.0;   //swerve wheel pivoting in driver control and auton
 const double angle_kI_left = 0.00;
@@ -125,7 +140,6 @@ const double distance_kP = 0.4; //swerve wheel rotation distance for auton
 const double distance_kI = 0.0;
 const double distance_kD = 500.0;
 
-const double MAX_VOLTAGE = 12800;
 
 const double velocity_kP = 0.002;   //swerve wheel rotation velocity for driver
 const double velocity_kI = 0.000;   //tune for translate
@@ -135,7 +149,7 @@ const double azim_kP = 0.03; //azimuth, for correcting rotation
 const double azim_kI = 0.0;    //drunk
 const double azim_kD = 10.0;
 
-const double ANGULAR_THRESH = 0.000; // Threshold under which to ignore angular error
+const double ANGULAR_THRESH = 0.0; // Threshold under which to ignore angular error
 
 const double r_kF = 0.0;   //feedforward compensation for rotation //flick
 const double r_kF_STATIC = 0.7; //FF STATIC for rotation
@@ -166,9 +180,37 @@ bool liftEnable = false;
 bool isLeftFlipped = false;
 bool isRightFlipped = false;
 
+//Slam dunk
+enum SlammingState {
+    SLAM_START_STATE = 0,
+    SLAM_MID_STATE = 1,
+    SLAM_EXTENDED_STATE = 2
+};
+SlammingState slammingState = SLAM_START_STATE;
+
+bool slam_dunk_actuated = false;
+
+double slam_target = 0;
+double slam_Kp = 0.31;
+double slam_Kd = 0.2;
+double slam_Ki = 0.0;
+
 //Serial
 double global_distY = 0.0;
 double global_distX = 0.0;
 double global_errorY = 0.0;
 double global_errorX = 0.0;
 
+//Mobile goal grabber
+bool mobile_goal_actuated = false;
+bool mobile_goal_jaw = false;
+
+bool driver = false;
+
+//Optical flow
+const double ALPHA = 0.85;
+const double BETA = 0.38;
+const double THRESHOLD = 200.0;
+const double height_from_gnd = 20.0;    //Height in mm
+const double scaler = 7.2;              //Adjust for sensitivity for different surfaces
+const double scale_factor = height_from_gnd * 2.0 * tan(42.0 / 2.0) / (35.0 * scaler);
