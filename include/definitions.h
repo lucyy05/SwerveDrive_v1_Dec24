19bc5ad56b16099c8cbca3c091089502d88b7d84
TKM <spyrogun1@gmail.com>
2024-12-25 00:56:09 +0800

    Squashed commit of the following:
    commit 5dd7e3cf73dbbff8c209ad4c0dc19dcb0380c4b0
    Author: TKM <spyrogun1@gmail.com>
    Date:   Wed Dec 25 00:54:38 2024 +0800
    
        Added rotation auton
    
    commit 10d682e7faff8409929d2cf6537be17238fa95ad
    Author: TKM <spyrogun1@gmail.com>
    Date:   Tue Dec 24 09:48:04 2024 +0800
    
        wtf
    
    commit 023d04f0c3e91f56f966aa60fe9d75485b4730a2
    Author: TKM <spyrogun1@gmail.com>
    Date:   Tue Dec 24 05:49:59 2024 +0800
    
        added rotation auton pid
    
    commit 79711171664c0613a89dbf27f984502925b60836
    Author: TKM <spyrogun1@gmail.com>
    Date:   Tue Dec 24 04:36:01 2024 +0800
    
        edited defs
    
    commit 0e08847e8c2dc57cdb04e342b309ee772bc735e3
    Merge: b2f33c7 7f9a8de
    Author: lucy <zihui0329@gmail.com>
    Date:   Tue Dec 24 04:19:32 2024 +0800
    
        merge commit
    
    commit b2f33c798d2ee9830b5a4854ba0a225d43c78fd5
    Author: TKM <spyrogun1@gmail.com>
    Date:   Tue Dec 24 04:04:24 2024 +0800
    
        update optical flow auton v1
    
    commit 7f9a8de20c8304db760a6fd16f0096ecfd544912
    Author: lucy <zihui0329@gmail.com>
    Date:   Tue Dec 24 04:03:12 2024 +0800
    
        time for more tuning, cleaned some code
    
    commit 33838074e9d62f745e79755659aa5d072dca843b
    Author: TKM <spyrogun1@gmail.com>
    Date:   Tue Dec 24 03:48:31 2024 +0800
    
        Updated optical flow auton for forward and backwards movement
    
    commit 530627220768ad1515e485384a0e54049fcaaa66
    Author: TKM <spyrogun1@gmail.com>
    Date:   Tue Dec 24 03:24:11 2024 +0800
    
        Added optical flow autonomous
    
    commit 67323e9e115382e7033a7a1822465bfb47bf86ee
    Author: lucy <zihui0329@gmail.com>
    Date:   Tue Dec 24 03:13:06 2024 +0800
    
        temp commit by john, working on testbase

--- include/definitions.h
+++ include/definitions.h
@@ -18,7 +18,7 @@
 #define RIGHT_UPPER_BEVEL_MOTOR_2 10
 #define RIGHT_LOWER_BEVEL_MOTOR_1 4   //ROBOT FRONT
 #define RIGHT_LOWER_BEVEL_MOTOR_2 5
-#define IMU_PORT 2
+#define IMU_PORT 13
 #define LEFT_ROTATION_SENSOR_PORT 18
 #define RIGHT_ROTATION_SENSOR_PORT 8
 
@@ -35,18 +35,18 @@
 // #define LEFT_ROTATION_SENSOR_PORT 18
 // #define RIGHT_ROTATION_SENSOR_PORT 8
 
-/* test base */
-//#define LEFT_UPPER_BEVEL_MOTOR_1 20 //ROBOT BACK
-//#define LEFT_UPPER_BEVEL_MOTOR_2 19
-//#define LEFT_LOWER_BEVEL_MOTOR_1 11 //ROBOT FRONT
-//#define LEFT_LOWER_BEVEL_MOTOR_2 12 
-//#define RIGHT_UPPER_BEVEL_MOTOR_1 8 //ROBOT BACK
-//#define RIGHT_UPPER_BEVEL_MOTOR_2 9
-//#define RIGHT_LOWER_BEVEL_MOTOR_1 2 //ROBOT FRONT
-//#define RIGHT_LOWER_BEVEL_MOTOR_2 3
-//#define LEFT_ROTATION_SENSOR_PORT 13
-//#define RIGHT_ROTATION_SENSOR_PORT 1
-//#define IMU_PORT 17
+/* test base (NO PAYLOAD)*/
+// #define LEFT_UPPER_BEVEL_MOTOR_1 16   //ROBOT BACK
+// #define LEFT_UPPER_BEVEL_MOTOR_2 17
+// #define LEFT_LOWER_BEVEL_MOTOR_1 14   //ROBOT FRONT
+// #define LEFT_LOWER_BEVEL_MOTOR_2 15
+// #define RIGHT_UPPER_BEVEL_MOTOR_1 9   //ROBOT BACK
+// #define RIGHT_UPPER_BEVEL_MOTOR_2 10
+// #define RIGHT_LOWER_BEVEL_MOTOR_1 4   //ROBOT FRONT
+// #define RIGHT_LOWER_BEVEL_MOTOR_2 5
+// #define IMU_PORT 13
+// #define LEFT_ROTATION_SENSOR_PORT 18
+// #define RIGHT_ROTATION_SENSOR_PORT 8
 
 #define SLAM_DUNK_SENSOR_PORT 'A'
 #define SLAM_DUNK_SOLENOID 'B'
@@ -64,6 +64,8 @@
 
 #define SERIALPORT 20
 
+#define SLAM_DUNK_MOTOR 3
+
 #define ZERO_VECTOR INFINITY
 
 pros::Controller master(pros::E_CONTROLLER_MASTER);
@@ -80,7 +82,7 @@ pros::Motor rlB(RIGHT_LOWER_BEVEL_MOTOR_2, pros::E_MOTOR_GEARSET_06, false, pros
 pros::IMU imu(IMU_PORT);
 pros::Optical colorSensor(COLOR_SENSOR);
 
-pros::Motor slam_dunkkkk(SLAM_DUNK_MOTOR, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor slam_dunk_motor(SLAM_DUNK_MOTOR, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::ADIDigitalOut slam_in_out(SLAM_DUNK_SOLENOID);
 pros::ADIAnalogIn slam_dunk(SLAM_DUNK_SENSOR_PORT);
 pros::ADIDigitalOut solenoid(SOLENOID_SENSOR_PORT);
@@ -108,10 +110,11 @@ extern "C" void vexGenericSerialEnable(  uint32_t index, uint32_t nu );
 extern "C" void vexGenericSerialBaudrate(  uint32_t index, uint32_t rate );
 extern "C" int32_t vexGenericSerialTransmit( uint32_t index, uint8_t *buffer, int32_t length );
 
-//Controllers
-int leftX = 0, leftY = 0, rightX = 0;
+/* Controllers */
+int leftX = 0, leftY = 0, rightX = 0, rightY=0;
 
-//PARAMETERS
+
+/* Parameters START */
 const double DEADBAND =  8.0;
 const double MAX_RPM = 600.0;
 const double TRANSLATE_RATIO = 1.0;
@@ -121,12 +124,10 @@ const double WHEEL_BASE_RADIUS = 161.50;    // mm
 const double MAX_SPEED = (2.0*M_PI*WHEEL_RADIUS*MAX_RPM)/60.0;  //mm per second
 const double SPEED_TO_RPM = 60.0/(2.0*M_PI*WHEEL_RADIUS);
 const double MAX_ANGULAR = MAX_SPEED/WHEEL_BASE_RADIUS; // rad/s
-const double ACCEL = 8000.0;    // mm/s2
-const double ANGULAR_ACCEL = 1.0;   // rad/s2
-const double SCALING_FACTOR = MAX_RPM / 127.0;
+const double MAX_ANGULAR_SCALE = 0.8;
 const double TO_DEGREES = (180.0 / M_PI);
 const double TO_RADIANS = (M_PI / 180.0);
-const double THETA_MAX = 15.0;
+
 
 //moving (moveBase)
 vector3D target_v;
@@ -135,33 +136,79 @@ vector3D temp;
 vector3D v_right;
 vector3D v_left;
 
-const double angle_kP_left = 20.0;   //swerve wheel pivoting in driver control and auton
-const double angle_kI_left = 0.00;
+
+//voltages
+int32_t lu; // left upper 
+int32_t ll; // left lower 
+int32_t ru; // right upper 
+int32_t rl; // right lower 
+
+/* Driver constants START */
+// Swerve wheel pivoting
+const double angle_kP_left = 20.0;
+const double angle_kI_left = 0.0;
 const double angle_kD_left = 5000.0;
 
-const double angle_kP_right = 20.0;   //swerve wheel pivoting in driver control and auton
-const double angle_kI_right = 0.00;
+const double angle_kP_right = 20.0;
+const double angle_kI_right = 0.0;
 const double angle_kD_right = 5000.0;
 
-const double distance_kP = 0.4; //swerve wheel rotation distance for auton
+const double velocity_kP = 0.002;   //swerve wheel rotation velocity for driver
+const double velocity_kI = 0.0;     //tune for translate
+const double velocity_kD = 160.0;
+
+const double distance_kP = 50.0; //swerve wheel rotation distance
 const double distance_kI = 0.0;
 const double distance_kD = 500.0;
+/* Driver constants END */
+
+/* Autonomous constants START */
+// Swerve wheel pivoting
+const double auton_angle_kP_left = 45.0;
+const double auton_angle_kI_left = 0.0;
+const double auton_angle_kD_left = 5000.0;
+
+const double auton_angle_kP_right = 45.0;
+const double auton_angle_kI_right = 0.0;
+const double auton_angle_kD_right = 5000.0;
+
+const double auton_l_velocity_kP = 0.05;   //swerve wheel rotation velocity for auton
+const double auton_l_velocity_kI = 0.000;     //tune for translate
+const double auton_l_velocity_kD = 0.02;
+
+const double auton_r_velocity_kP = 0.05;   //swerve wheel rotation velocity for auton
+const double auton_r_velocity_kI = 0.000;     //tune for translate
+const double auton_r_velocity_kD = 0.02;
+
+const double auton_distance_kP = 0.05; //swerve wheel rotation distance
+const double auton_distance_kI = 0.0;
+const double auton_distance_kD = 0.0;
+
+double auton_heading_kP = 0.09; //swerve heading
+double auton_heading_kI = 0.0;
+double auton_heading_kD = 0.05;
+
+enum AutonDirections {
+    NORTH = 0,
+    SOUTH = 1,
+    EAST = 2,
+    WEST = 3,
+    NORTHEAST = 4,
+    NORTHWEST = 5,
+    SOUTHEAST = 6,
+    SOUTHWEST = 7
+};
+
+AutonDirections autonDirection;
+/* Autonomous constants END */
 
 const double MAX_VOLTAGE = 12000;
 
-const double velocity_kP = 0.002;   //swerve wheel rotation velocity for driver
-const double velocity_kI = 0.000;   //tune for translate
-const double velocity_kD = 160;
-
-const double azim_kP = 0.03; //azimuth, for correcting rotation
+const double azim_kP = 0.05; //azimuth, for correcting rotation
 const double azim_kI = 0.0;    //drunk
 const double azim_kD = 10.0;
 
-const double translate_kP = 0.81; 
-const double translate_kI = 0.0;
-const double translate_kD = 0.01;
-
-const double ANGULAR_THRESH = 0.000; // Threshold under which to ignore angular error
+const double ANGULAR_THRESH = 0.0; // Threshold under which to ignore angular error
 
 const double r_kF = 0.2;   //feedforward compensation for rotation //flick
 const double r_kF_STATIC = 0.7; //FF STATIC for rotation
@@ -204,19 +251,31 @@ SlammingState slammingState = SLAM_START_STATE;
 
 bool slam_dunk_actuated = false;
 
-double slam_target = 0;
+double slam_target = 0.0;
 double slam_Kp = 0.31;
 double slam_Kd = 0.2;
 double slam_Ki = 0.0;
 
-//Serial
+//Serial read
 double global_distY = 0.0;
 double global_distX = 0.0;
 double global_errorY = 0.0;
 double global_errorX = 0.0;
+double optical_v_x = 0.0;
+double optical_v_y = 0.0;
 
 //Mobile goal grabber
 bool mobile_goal_actuated = false;
 bool mobile_goal_jaw = false;
 
+//Optical flow
+const double ALPHA = 0.85;
+const double BETA = 0.38;
+const double THRESHOLD = 200.0;
+const double height_from_gnd = 20.0;    //Height in mm
+const double scaler = 7.2;              //Adjust for sensitivity for different surfaces
+const double scale_factor = height_from_gnd * 2.0 * tan(42.0 / 2.0) / (35.0 * scaler);
+
+//Modes
 bool driver = false;
+bool arcade = true;
