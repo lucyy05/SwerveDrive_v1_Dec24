80b75ca1abd4e8eac7bfe96bfe9ac594e25572ed
KidSmithy <hanshuennyuen@gmail.com>
2024-12-27 08:03:07 +0800

    add yoinker

--- include/definitions.h
+++ include/definitions.h
@@ -57,6 +57,7 @@
 #define SOLENOID_SENSOR_PORT 'D'
 #define mobilegoal_bottom 'G'
 #define POTENTIOMETER_SENSOR_PORT 'H'
+#define Yoinker_SENSOR_PORT 'F'
 
 #define CONVEYOR_OPTICAL 1
 #define CONVEYOR_THRES_PROX 130
@@ -89,6 +90,7 @@ pros::ADIDigitalOut slam_in_out(SLAM_DUNK_SOLENOID);
 pros::ADIAnalogIn slam_dunk(SLAM_DUNK_SENSOR_PORT);
 pros::ADIDigitalOut solenoid(SOLENOID_SENSOR_PORT);
 pros::ADIDigitalOut mobilegoal_bot(mobilegoal_bottom);
+pros::ADIDigitalOut yoinker(Yoinker_SENSOR_PORT);
 
 // pros::Motor intakeLower(UPPER_INTAKE_MOTOR, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES);
 // pros::Motor intakeUpper(LOWER_INTAKE_MOTOR, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES);
